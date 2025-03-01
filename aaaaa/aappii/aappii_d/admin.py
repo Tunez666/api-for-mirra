5f027e5126f57d224143ae03368a2aa50bafe575
Tunez <163091834+Tunez666@users.noreply.github.com>
2025-03-01 14:17:18 +0800

    Delete aaaaa directory

--- aaaaa/aappii/aappii_d/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import arrr
-
-@admin.register(arrr)
-class ArrrAdmin(admin.ModelAdmin):
-    list_display = ('title', 'token', 'key')
-    search_fields = ('title', 'token')
-    readonly_fields = ('key',)
--- aaaaa/aappii/aappii_d/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AappiiDConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aappii_d'
