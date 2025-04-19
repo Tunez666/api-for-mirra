38863dfef87889215dd226f6875d6ae4da3cb110
Tunez <163091834+Tunez666@users.noreply.github.com>
2025-04-19 12:53:54 +0800

    Delete PythonProject/costar_project directory

--- PythonProject/costar_project/nursing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import buy_ticket_view, ticket_pdf
-
-urlpatterns = [
-    path('buy/', buy_ticket_view, name='buy-ticket'),
-    path('ticket/<int:ticket_id>/pdf/', ticket_pdf, name='ticket-pdf'),
-]
